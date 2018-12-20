163e8d6712471931a730cf606b00591174d41f7a
Kasumi6 <45140371+Kasumi6@users.noreply.github.com>
2018-12-20 20:49:49 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -9,7 +9,7 @@ function random(min, max){
 }
 
 client.on("ready", () => {
-    client.user.setActivity(`A?help | 26 serveurs`,{type:"PLAYING"})
+    client.user.setActivity(`A?help | ${client.guilds.size} serveurs !`,{type:"PLAYING"})
     console.log('')
     console.log('')
     console.log('+[-----------------------------------------------------------------]+')
